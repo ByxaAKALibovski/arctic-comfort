2ce7d44fc3cdfad1fb55cea15ce19bd1468874a2
ByxaAKALibovski <90106583+ByxaAKALibovski@users.noreply.github.com>
2024-04-02 20:20:45 +0300

    Add files via upload

---
+++ api/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>API arctic-comfort</title>
+    <style>
+
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    body,html{
+        width: 100%;
+        height: 100vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background: linear-gradient(-45deg, #03544c, #050b1a);
+    }
+
+    h1{
+        font-size: 30px;
+        color: #fff;
+        font-family: Arial;
+    }
+
+    </style>
+</head>
+<body>
+    <h1>Вас привествует API сайта Арктический Комфорт!</h1>
+</body>
+</html>
